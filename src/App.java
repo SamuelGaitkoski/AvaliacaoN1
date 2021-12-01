833c0510f471c8f6b7569d147be24f1f043cb94c
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2021-11-30 21:23:48 -0300

    Jogo corrigido

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
     
-        int numero = 0;
+        String opcao = "", escolhaComputador = "";
         Scanner teclado = new Scanner(System.in);
         System.out.println();
         System.out.println("Bem-vindo ao jogo de pedra, papel e tesoura");
@@ -13,11 +13,11 @@ public class App {
         var escolhaUsuario = teclado.nextInt();
 
         if (escolhaUsuario == 1) {
-            numero = 1;
+            opcao = "Pedra";
         } else if (escolhaUsuario == 2) {
-            numero = 2;
+            opcao = "Papel";
         } else if (escolhaUsuario == 3) {
-            numero = 3;
+            opcao = "Tesoura";
         } else {
             System.out.println("Opção inválida!");
             System.exit(0);
@@ -29,14 +29,22 @@ public class App {
         Random randon = new Random();
         int numeroComputador = randon.nextInt(max) + min;
 
+        if (numeroComputador == 1) {
+            escolhaComputador = "Pedra";
+        } else if (numeroComputador == 2) {
+            escolhaComputador = "Papel";
+        } else {
+            escolhaComputador = "Tesoura";
+        } 
+
         Jogo jogo = new Jogo();
         System.out.println();
-        System.out.printf("Resultado da rodada: %s", jogo.compararValores(escolhaUsuario, numeroComputador));
+        System.out.printf("Resultado da rodada: %s", jogo.verificarEscolhas(escolhaUsuario, numeroComputador));
 
         System.out.println();
-        System.out.printf("Peso da sua escolha: %d", numero);
+        System.out.printf("Sua escolha: %s", opcao);
         System.out.println();
-        System.out.printf("Peso da escolha do seu oponente: %d", numeroComputador);
+        System.out.printf("Escolha do seu oponente: %s", escolhaComputador);
 
 
         teclado.close();
--- src/Jogo.java
+++ src/Jogo.java
@@ -1,14 +1,40 @@
 
 public class Jogo {
 
-    public String compararValores(int escolhaUsuario, int numeroComputador) {
+    public String verificarEscolhas(int escolhaUsuario, int numeroComputador) {
+        if(escolhaUsuario == 1 && numeroComputador == 1) {
+            return "Empate";
+        }
 
-        if(numeroComputador == escolhaUsuario) {
-            return "Empate!";
-        } else if (escolhaUsuario > numeroComputador) {
-            return "Você venceu!";
-        } else {
+        if(escolhaUsuario == 1 && numeroComputador == 2) {
             return "Seu oponente venceu!";
         }
+
+        if(escolhaUsuario == 1 && numeroComputador == 3) {
+            return "Você venceu!";
+        }
+
+        if(escolhaUsuario == 2 && numeroComputador == 1) {
+            return "Você venceu!";
+        }
+
+        if(escolhaUsuario == 2 && numeroComputador == 2) {
+            return "Empate";
+        }
+
+        if(escolhaUsuario == 2 && numeroComputador == 3) {
+            return "Seu oponente venceu!";
+        }
+
+        if(escolhaUsuario == 3 && numeroComputador == 1) {
+            return "Seu oponente venceu!";
+        }
+
+        if(escolhaUsuario == 3 && numeroComputador == 2) {
+            return "Você venceu!";
+        } else {
+            return "Empate";
+        }
+
     }
 }
